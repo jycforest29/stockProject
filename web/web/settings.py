3ee3e571741c53102d7f03b959c8b99517ad2b9d
jycforest29 <jycforest29.dev@gmail.com>
2022-05-11 13:53:48 +0900

    장고 시크릿키 숨김

--- web/web/settings.py
+++ web/web/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1=w(#^h!a4y%qw8fm(!1q-^_ocsse_s9udi$h56%2-iy0cfuv='
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
